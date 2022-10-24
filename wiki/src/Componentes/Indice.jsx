9bfa34509c84bbb2a1094f67412144881a57d068
Luis Hernández <luisjunior9831@gmail.com>
2022-10-24 15:00:23 -0500

    Agregando Archivos

---
+++ wiki/src/Componentes/Indice.jsx
@@ -0,0 +1,17 @@
+function Indice(){
+    return <>
+
+    
+    <div className="indice">
+    <h5>Indice <a href="#">[ocultar]</a></h5>
+    <ol>
+        <li><a href="#">Compocision</a></li>
+        <li><a href="#">Estructura</a></li>
+        <li><a href="#">Grabacion</a></li>
+        <li><a href="#">Referencias Bibliograficas</a></li>
+        <li><a href="#">Referencias</a></li>
+    </ol>
+    </div>
+    </>
+}
+export default Indice
